cfa8bf23fe15adab7c2af6ffd8f49d5f658e6141
Emmanuel López Guerrero <manelpz@icloud.com>
2018-11-19 23:37:30 -0600

    Challenge 2
    First commit challenge

---
+++ Udemy - Challenge 2 - The Complete JavaScript Course 2018: Build Real Projects.js
@@ -0,0 +1,22 @@
+var a=122;
+var b=22;
+var c=3;
+
+if (a>b)
+{
+  console.log("A es mayor")
+}else{
+  console.log("B es mayor")
+}
+
+
+if (a>b && a>c){
+  console.log("el mayor es A");
+}else if (b>a && b>c){
+  console.log("el mayor es B");
+}else if (c>a && c>b){
+  console.log("el mayor es C");
+}else {
+  console.log("there is a draw");
+}
+
