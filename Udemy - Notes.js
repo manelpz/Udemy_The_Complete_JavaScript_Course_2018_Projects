7e51eb8c1d67f89c5f8b7a5d1e5b1cb8bb6973d9
Emmanuel López Guerrero <manelpz@icloud.com>
2020-06-21 18:27:48 -0500

    hoisting

--- Udemy - Notes.js
+++ Udemy - Notes.js
@@ -1,7 +1,7 @@
 
 
 //objects js
-
+/*
 var john = {
     firstName: 'John',
     lastName:'Smith',
@@ -22,3 +22,29 @@ for (var i = 0; i < john.length; i++){
     if(typeof john[i] == 'string') continue; //break;
     console.log(john[i]);
 }
+*/
+
+//hoisting
+
+//call fistly a function is possible for hoisting
+calculateAge(1990);
+
+function calculateAge(year){
+    console.log(2016 - year);
+}
+
+ //function expression
+ //function expression isnt possible to call with hoisting
+ //retirement(1990);
+ // all depent of execution context
+ 
+ var retirement  = function(year){
+     console.log(65 - (2020 - year));
+ }
+
+
+
+/*
+ console.log(age);
+ var age =30;*/
+
