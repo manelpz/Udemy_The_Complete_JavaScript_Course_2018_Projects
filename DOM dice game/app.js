1ac7100e5c6cde44dd892d477d7683f50e8158c0
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-05 23:38:50 -0500

    adding dice event

--- DOM dice game/app.js
+++ DOM dice game/app.js
@@ -20,3 +20,13 @@ var scores, roundScore, activePlayer, dise;
 //document.querySelector('#current-'+activiPlayer).textContent = dice;
 document.querySelector('.dice').style.display = 'none';
 
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+    //1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+dice+'.png';
+})
