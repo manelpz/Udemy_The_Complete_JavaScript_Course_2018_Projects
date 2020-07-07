979dde93751b2c340fc9a57f174e1119616e48da
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-06 21:37:11 -0500

    fixing bugs

--- DOM dice game/app.js
+++ DOM dice game/app.js
@@ -9,32 +9,34 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
-//scores = [0,0];
+scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-//dice = Math.floor(Math.random() * 6) + 1;console.log(dice);
-
-//document.querySelector('#current-'+activiPlayer).textContent = dice;
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
 
     //1. random number
-    dice = Math.floor(Math.random() * 6) + 1;
+    var dice = Math.floor(Math.random() * 6) + 1;
 
     //2. Display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-'+dice+'.png';
 
+
     //3. Update the round score if the rolled number was not a 1
     if(dice !== 1){
         //add score
-        roundScore = dice + roundScore;
-        console.log('actualiza '+roundScore);
+        roundScore += dice;
         document.querySelector('#current-'+ activePlayer).textContent = roundScore;
     } else{
         //next player
@@ -52,9 +54,9 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     //check if player won the game
-if(scores[activePlayer] >= 20){
-    document.querySelector('#name-'+ activePlayer).textContent='Winner';
-}else{
+    if(scores[activePlayer] >= 20){
+        document.querySelector('#name-'+ activePlayer).textContent='Winner';
+    }else{
     //next players
     nextPlayer();
     }
@@ -63,11 +65,11 @@ if(scores[activePlayer] >= 20){
 function nextPlayer(){
 
     //next player
-    activePlayer === 0 ? activiPlayer =1 :activePlayer =0;
+    activePlayer === 0 ? activePlayer =1 :activePlayer =0;
     roundScore=0;
 
-        document.getElementById('#current-0').textContent = '0';
-        document.getElementById('#current-1').textContent = '0';
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
         document.querySelector('.dice').style.display = 'none';
