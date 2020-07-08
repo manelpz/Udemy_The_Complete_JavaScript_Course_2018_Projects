7f310544783d6c20541bc5782ab91ade0936ba4f
Emmanuel López Guerrero <manelpz@icloud.com>
2020-07-07 22:56:10 -0500

    adding init()

--- DOM dice game/app.js
+++ DOM dice game/app.js
@@ -11,16 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
+function init();
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
 
@@ -77,3 +68,17 @@ function nextPlayer(){
         document.querySelector('.player-1-panel').classList.toggle('active');
         document.querySelector('.dice').style.display = 'none';
 }
+
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    
+    document.querySelector('.dice').style.display = 'none';
+    
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    
+} 
